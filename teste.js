c1f9af5555246c67d6b217d913a1f36062b5f835
Marc-Web <marconieng@gmail.com>
2020-10-17 08:29:13 -0300

    Aterações no arquivo teste.js

--- teste.js
+++ teste.js
@@ -1,8 +1,4 @@
 let student = "euMesmo";
 let habilidadesPessoaEstudante = 10;
-javascript-updates
 //Eu sou Tryber e vou ter muito sucesso na programação!
-=======
-//Eu sou Tryber e...
- master
 <console.log("Sucesso!");
