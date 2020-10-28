2aa0a397ba4eff9086b82d26ea96d33024739bfc
Marconi <marconieng@gmail.com>
2020-10-28 08:39:57 -0300

    Adiciona maiordetresnumeros.js

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/maiordetresnumeros.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/maiordetresnumeros.js
@@ -1,4 +1,14 @@
-let numero = 7
-let numero = 10
-let numero = 4
+let num1 = 7
+let num2 = 10
+let num3 = 19
 
+function max(num1, num2, num3) {
+
+    if(num1 > num2 || num1 > num3)
+        console.log(num1);
+    
+    if(num2 > num1 || num2 > num3)
+        console.log(num2);
+    
+    else console.log(num3);
+}        
