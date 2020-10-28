2077f4eb66856005894eac4f3ed499a558df7b26
Marconi <marconieng@gmail.com>
2020-10-28 11:51:50 -0300

    Adiciona o exercício 6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exrcise6.js
@@ -0,0 +1,25 @@
+let pecaDeXadrez = "Bispo";
+
+    switch (pecaDeXadrez.toLowerCase) {
+        case "Rei":
+            console.log("Rei - uma casa para qualquer direção.");
+            break;
+        case "Bispo":
+            console.log("Bispo - diagonal.");
+            break;
+        case "Rainha" :
+            console.log("Rainha - diagonal e horizontal.");
+            break;
+        case "Cavalo" :
+            console.log("Cavalo - L, pode pular sobre as peças.");
+            break;
+        case "Peão" :
+            console.log("Peão - apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+            break;
+        case "Torre" :
+            console.log("Torre - horizontal.");
+            break;
+        default:
+            console.log("Erro, peça inválida.");
+            break;
+    }
