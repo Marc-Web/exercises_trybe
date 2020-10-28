d38f1823077998a22d2ef0c2ecd8659a7c37c2fd
Marconi <marconieng@gmail.com>
2020-10-28 08:57:55 -0300

    Adiciona alteração nas condicionais

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/maiordetresnumeros.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/maiordetresnumeros.js
@@ -1,14 +1,14 @@
-let num1 = 7
+let num1 = 2
 let num2 = 10
-let num3 = 19
+let num3 = 3
 
-function max(num1, num2, num3) {
 
-    if(num1 > num2 || num1 > num3)
-        console.log(num1);
-    
-    if(num2 > num1 || num2 > num3)
-        console.log(num2);
-    
-    else console.log(num3);
-}        
+if(num1 > num2 && num1 > num3) {
+    console.log(num1);
+}
+else if(num2 > num1 && num2 > num3) {
+    console.log(num2);
+}
+else {
+    console.log(num3);
+}    
