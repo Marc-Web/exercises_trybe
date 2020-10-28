330ed6b95955a15955fc1d5288414db37ab402ae
Marconi <marconieng@gmail.com>
2020-10-28 09:28:38 -0300

    Adiciona o exercício 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
@@ -0,0 +1,10 @@
+let angulo1 = 100;
+let angulo2 = 60;
+let angulo3 = 20;
+
+let somaDosAngulos = angulo1 + angulo2 + angulo3;
+if(somaDosAngulos === 180) {
+    console.log("Ângulos válidos.")
+} else {
+    console.log("Ângulos inválidos.")
+}
