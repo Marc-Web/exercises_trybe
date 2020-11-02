681e581a79fecc2dcc3dc642ce4513424428d96a
Marconi <marconieng@gmail.com>
2020-11-01 22:25:24 -0300

    Adiciona o exercício 2

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise2.js
@@ -0,0 +1,9 @@
+let tamanho = 5;
+let linha;
+let simbolo = "*";
+let linhaInicial = "";
+
+for(linha = 0; linha <= tamanho; linha += 1) {
+    console.log(linhaInicial);
+    linhaInicial = linhaInicial + simbolo;
+}
